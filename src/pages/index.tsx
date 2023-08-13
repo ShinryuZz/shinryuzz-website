968c702fe8c8e842ad9797b9b0acb514667c3998
Shinryuzz <mshin1127@gmail.com>
2023-08-13 13:40:42 +0900

    add google font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-dark-50 text-3xl">Welcome to shinryuzz's website</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10">
+      <h1 className="text-3xl">Welcome to shinryuzz's website 🎉</h1>
+      <h2 className="text-xl">こんにちは、しんりゅうです</h2>
     </main>
   );
 }
