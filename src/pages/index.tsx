02288a921c846f78a0e3aa612fcd32fa0cc7c17d
Shinryuzz <mshin1127@gmail.com>
2023-08-13 15:14:54 +0900

    add layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-10">
-      <h1 className="text-3xl">Welcome to shinryuzz's website 🎉</h1>
+    <div className="flex flex-col items-center gap-5">
+      <h1 className="text-3xl">Welcome to shinryuzz&apos;s website 🎉</h1>
       <h2 className="text-xl">こんにちは、しんりゅうです</h2>
-    </main>
+    </div>
   );
 }
