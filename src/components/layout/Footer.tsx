f62aa6b2d0daecc40d1d685551014affbb330182
Shinryuzz <mshin1127@gmail.com>
2023-08-14 13:14:51 +0900

    add mdx page and fix footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   const personalTwitterURL: string = "https://twitter.com/shinryuslpr1127";
   return (
-    <footer className="fixed bottom-0 w-full p-5 border-t border-darker-400 flex flex-col justify-center items-center ">
+    <footer className="w-full p-5 border-t border-darker-400 flex flex-col justify-center items-center ">
       <span className="text-darker-50 sm:text-center">
         &copy; 2023{" "}
         <a
