a8ccb77577ec0d2742e21a78df65b77893797026
Shinryuzz <mshin1127@gmail.com>
2023-08-15 09:54:44 +0900

    fix footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,20 +1,54 @@
 import React from "react";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   const personalTwitterURL: string = "https://twitter.com/shinryuslpr1127";
+  const personalGithubURL: string = "https://github.com/shinryuzz";
   return (
-    <footer className="w-full p-5 flex flex-col justify-center items-center">
-      <span className="text-darker dark:text-light">
-        &copy; 2023{" "}
+    <footer className="w-full p-5 flex flex-col justify-center items-center gap-3">
+      <div className="flex gap-8 items-center">
         <a
           href={personalTwitterURL}
           target="_blank"
-          className="hover:underline hover:text-cyan-800"
+          className="text-[#1DA1F2] hover:opacity-70"
         >
-          shinryuzz
+          <FontAwesomeIcon icon={faTwitter} size="lg" />
         </a>
-        . All Rights Reserved.
-      </span>
+        <a
+          href={personalGithubURL}
+          target="_blank"
+          className="text-[#24292E] dark:text-[#FAFBFC] hover:opacity-70"
+        >
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+        </a>
+      </div>
+
+      <div className="flex gap-3 text-sm">
+        <span className="text-darker dark:text-light">
+          &copy; 2023 shinryuzz. All Rights Reserved.
+        </span>
+
+        <span>|</span>
+
+        <span className="flex items-center gap-1">
+          Powered by{" "}
+          <a
+            href="https://vercel.com/home"
+            target="_blank"
+            className="hover:opacity-70"
+          >
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={64}
+              height={24}
+            />
+          </a>
+        </span>
+      </div>
     </footer>
   );
 };
