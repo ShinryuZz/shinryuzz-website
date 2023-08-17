66053190ee7053467e20d4f6c05c6e451b46160f
Shinryuzz <mshin1127@gmail.com>
2023-08-17 11:25:42 +0900

    quit use of mdx

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
-const remarkPrism = require("remark-prism");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-// const withMDX = require("@zeit/next-mdx")({
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    rehypePlugins: [],
-    remarkPlugins: [remarkPrism],
-    providerImportSource: "@mdx-js/react",
-  },
-});
-
 module.exports = nextConfig;
-
-module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-});
