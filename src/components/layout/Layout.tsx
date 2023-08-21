e2ddec0e158b4f774683672a16ff2625eec9707d
Shinryuzz <mshin1127@gmail.com>
2023-08-21 21:58:05 +0900

    add Meta component and fix icons

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Meta from "./Meta";
 
 type Props = {
   children: React.ReactNode;
@@ -8,24 +9,30 @@ type Props = {
 
 export const NormalLayout = ({ children }: Props) => {
   return (
-    <div className="flex flex-col min-h-screen text-darker dark:text-light bg-light dark:bg-darker">
-      <Header />
-      <main className="sm:w-2/3 max-w-2xl md:mx-auto flex flex-col flex-grow items-center justify-start py-10 mx-10 sm:mt-20">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Meta />
+      <div className="flex flex-col min-h-screen text-darker dark:text-light bg-light dark:bg-darker">
+        <Header />
+        <main className="sm:w-2/3 max-w-2xl md:mx-auto flex flex-col flex-grow items-center justify-start py-10 mx-10 sm:mt-20">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
 
 export const MarkdownLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen text-darker dark:text-light bg-light dark:bg-darker">
-      <Header />
-      <main className="sm:w-2/3 max-w-2xl mx-auto px-10 sm:px-0">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Meta />
+      <div className="min-h-screen text-darker dark:text-light bg-light dark:bg-darker">
+        <Header />
+        <main className="sm:w-2/3 max-w-2xl mx-auto px-10 sm:px-0">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
---
+++ src/components/layout/Meta.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <title></title>
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+      {/* <link rel="manifest" href="/favicon/site.webmanifest" /> */}
+      {/* <meta property="og:image" content={} /> */}
+    </Head>
+  );
+};
+
+export default Meta;
