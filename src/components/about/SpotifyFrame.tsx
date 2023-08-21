649c009527733b25ba032134062edb92e2eb9044
Shinryuzz <mshin1127@gmail.com>
2023-08-22 07:48:55 +0900

    add SpotifyFrame component

---
+++ src/components/about/SpotifyFrame.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SpotifyFrame = () => {
+  return (
+    <iframe
+      src="https://open.spotify.com/embed/track/2kxp3ZUOXssozAGMEXshLO?utm_source=generator"
+      width="100%"
+      height="160"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+  );
+};
+
+export default SpotifyFrame;
