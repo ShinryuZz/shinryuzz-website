1b31f1248b8e1fe64e10a0bb8fa4c1e19f998dba
Shinryuzz <mshin1127@gmail.com>
2023-08-23 07:04:35 +0900

    fix SpotifyFrame component with spotify id constant

--- src/components/about/SpotifyFrame.tsx
+++ src/components/about/SpotifyFrame.tsx
@@ -1,9 +1,10 @@
+import { SPOTIFY_EMBED_ID } from "@/const/constants";
 import React from "react";
 
 const SpotifyFrame = () => {
   return (
     <iframe
-      src="https://open.spotify.com/embed/track/2kxp3ZUOXssozAGMEXshLO?utm_source=generator"
+      src={SPOTIFY_EMBED_ID}
       width="100%"
       height="160"
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
--- src/const/constants.ts
+++ src/const/constants.ts
@@ -3,3 +3,8 @@ export const WEBSITE_DESCRIPTION = "ソフトウェアエンジニア shinryuzz 
 export const WEBSITE_KEYWORDS =
   "blog, shinryuzz, software engineer, ブログ, ソフトウェアエンジニア";
 export const TWITTER_ID = "@shinryuzz__";
+
+export const SPOTIFY_EMBED_ID =
+  "https://open.spotify.com/embed/track/4hCLAWaQXvcEpUbGgyfEsX?utm_source=generator";
+
+// export const SPOTIFY_EMBED_ID = "https://open.spotify.com/embed/track/2kxp3ZUOXssozAGMEXshLO?utm_source=generator"
