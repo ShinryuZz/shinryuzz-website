eac1a200528190232ce8e6aee8fe4cbdeb70943d
Shinryuzz <mshin1127@gmail.com>
2023-08-24 09:51:45 +0900

    add GA4

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "";
   return (
     <Html lang="ja">
-      <Head></Head>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
