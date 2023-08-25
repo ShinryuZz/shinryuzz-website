e882509dec0016afc794453c385c70eaeeff7cfb
Shinryuzz <mshin1127@gmail.com>
2023-08-25 10:12:58 +0900

    update packages for markdown syntax highlight

--- src/lib/markdownToHtml.js
+++ src/lib/markdownToHtml.js
@@ -1,8 +1,17 @@
-import { remark } from "remark";
-import html from "remark-html";
-import prism from "remark-prism";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrism from "rehype-prism-plus";
+import rehypeCodeTitles from "rehype-code-titles";
 
 export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).use(prism).process(markdown);
+  const result = await unified()
+    .use(remarkParse) // markdown -> mdast の変換
+    .use(remarkRehype) // mdast -> hast の変換
+    .use(rehypeStringify) // hast -> html の変換
+    .use(rehypePrism) // prism の追加 (diffや行番号も)
+    .use(rehypeCodeTitles) // ファイル名表示の追加
+    .process(markdown); // 実行
   return result.toString();
 }
