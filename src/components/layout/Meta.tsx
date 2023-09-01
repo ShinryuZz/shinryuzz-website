9b6d64396000923e80a6ddbc2fd3bf4f08bc1914
Shinryuzz <mshin1127@gmail.com>
2023-09-01 09:09:32 +0900

    replace new favicons

--- src/components/layout/Meta.tsx
+++ src/components/layout/Meta.tsx
@@ -10,8 +10,28 @@ import {
 const Meta = () => {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
+
       <meta property="og:image" content={`${WEBSITE_URL}/og-image.svg`} />
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
