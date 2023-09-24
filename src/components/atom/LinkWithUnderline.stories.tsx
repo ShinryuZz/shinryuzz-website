f92cd7a0b3a6d50816f3f0fe7cbb8fb1e7910a48
Shinryuzz <mshin1127@gmail.com>
2023-09-24 23:44:33 +0900

    test: check storybook

---
+++ src/components/atom/LinkWithUnderline.stories.tsx
@@ -0,0 +1,14 @@
+import LinkWithUnderline from "./LinkWithUnderline";
+
+export default {
+  component: LinkWithUnderline,
+  title: "Title",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    href: "google.com",
+    text: "title",
+  },
+};
---
+++ src/components/atom/TitleWithUnderline.stories.tsx
@@ -0,0 +1,13 @@
+import TitleWithUnderline from "./TitleWithUnderline";
+
+export default {
+  component: TitleWithUnderline,
+  title: "TitleWithUnderline",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    title: "Hello",
+  },
+};
