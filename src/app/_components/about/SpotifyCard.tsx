ae84aa462a561855367fd531b28981c85e0807ba
Shinryuzz <mshin1127@gmail.com>
2024-06-25 10:44:58 +0900

    fix: update music and refactor spotify card

---
+++ src/app/_components/about/SpotifyCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import TitleWithUnderline from "../atoms/TitleWithUnderline";
+import { SPOTIFY_EMBED_ID } from "@/const/constants";
+
+const SpotifyFrame = () => {
+  return (
+    <iframe
+      src={SPOTIFY_EMBED_ID}
+      width="100%"
+      height="160"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+  );
+};
+
+const SpotifyCard = () => {
+  return (
+    <div className="w-full flex flex-col items-start gap-4">
+      <TitleWithUnderline title="Masterpiece 💿" />
+      <SpotifyFrame />
+    </div>
+  );
+};
+
+export default SpotifyCard;
--- src/const/constants.ts
+++ src/const/constants.ts
@@ -9,4 +9,5 @@ export const TWITTER_ID = "@shinryuzz__";
 export const AUTHOR = { name: "shinryuzz", url: WEBSITE_URL };
 
 export const SPOTIFY_EMBED_ID =
-  "https://open.spotify.com/embed/track/4hCLAWaQXvcEpUbGgyfEsX?utm_source=generator";
+  "https://open.spotify.com/embed/track/37J0tSBhYuE3F8j1pB1Vwr?si=6ebcc969c48742b4";
+// "https://open.spotify.com/embed/track/4hCLAWaQXvcEpUbGgyfEsX?utm_source=generator";
