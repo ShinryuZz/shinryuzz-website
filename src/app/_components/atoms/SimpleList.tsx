6d85ba709a04bcb8506dfc0808e06a6f6eb54c15
Shinryuzz <mshin1127@gmail.com>
2024-06-28 15:14:21 +0900

    fix: adjust ul for responsive

--- src/app/_components/atoms/SimpleList.tsx
+++ src/app/_components/atoms/SimpleList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const List = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ul className="flex flex-col gap-3 list-disc list-inside ml-2 text-red">
+    <ul className="flex flex-col flex-wrap gap-3 list-disc list-outside">
       {children}
     </ul>
   );
@@ -10,28 +10,25 @@ export const List = ({ children }: { children: React.ReactNode }) => {
 
 export const ListItem = ({ children }: { children: React.ReactNode }) => {
   return (
-    <li>
+    <li className="break-all ml-4 text-red">
       <span className="text-darker dark:text-light">{children}</span>
     </li>
   );
 };
 
-export const LinkItem = ({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href: string;
-}) => {
-  return (
-    <a
-      href={href}
-      className="text-darker dark:text-light hover:opacity-70 break-keep"
-    >
-      {children}
-    </a>
-  );
-};
+// export const LinkItem = ({
+//   children,
+//   href,
+// }: {
+//   children: React.ReactNode;
+//   href: string;
+// }) => {
+//   return (
+//     <a href={href} className="text-darker dark:text-light hover:opacity-70">
+//       {children}
+//     </a>
+//   );
+// };
 
 export const ListItemLink = ({
   children,
