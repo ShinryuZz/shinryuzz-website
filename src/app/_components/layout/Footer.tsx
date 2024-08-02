9b78636939a66f2fd24275a3cb0fc82716135346
Shinryuzz <mshin1127@gmail.com>
2024-08-03 07:45:53 +0900

    chore: remove footer icons

--- src/app/_components/layout/Footer.tsx
+++ src/app/_components/layout/Footer.tsx
@@ -7,28 +7,7 @@ const Footer = () => {
       <Link href="/policy" className="hover:text-primary ">
         Privacy Policy
       </Link>
-      <div className="flex gap-3 ">
-        <span>&copy; 2023 shinryuzz. All Rights Reserved.</span>
-
-        {/* <span className="hidden sm:block">|</span>
-
-        <span className="flex items-center gap-1">
-          Powered by{" "}
-          <a
-            href="https://vercel.com/home"
-            target="_blank"
-            className="hover:opacity-70"
-          >
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={64}
-              height={24}
-            />
-          </a>
-        </span> */}
-      </div>
+      <p>&copy; 2024 shinryuzz. All Rights Reserved.</p>
     </footer>
   );
 };
