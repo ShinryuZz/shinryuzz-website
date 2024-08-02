f723ede402ae96d1800ea0a23eb1a1188972b140
Shinryuzz <mshin1127@gmail.com>
2024-08-03 07:45:00 +0900

    feat: add scrapbox url

--- src/const/social-links.ts
+++ src/const/social-links.ts
@@ -9,6 +9,7 @@ const facebook: string =
   "https://www.facebook.com/profile.php?id=100012067954842";
 const instagram: string = "https://www.instagram.com/shinryuzz__/";
 const booklog: string = "https://booklog.jp/users/shinryuzz";
+const scrapbox: string = "https://scrapbox.io/shinryuzz";
 
 export const socialLinks: SocialLink[] = [
   { name: "Twitter", url: twitter },
@@ -16,4 +17,5 @@ export const socialLinks: SocialLink[] = [
   { name: "Facebook", url: facebook },
   { name: "Instagram", url: instagram },
   { name: "ブクログ", url: booklog },
+  { name: "Scrapbox", url: scrapbox },
 ];
