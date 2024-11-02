3ad1c70d60947e17bcdff2249c3c8650446a21c9
Shinryuzz <mshin1127@gmail.com>
2024-11-03 00:40:00 +0900

    feat: add zenn link

--- src/const/social-links.ts
+++ src/const/social-links.ts
@@ -5,19 +5,21 @@ export type SocialLink = {
 
 export const twitter: string = "https://twitter.com/shinryuzz__";
 export const github: string = "https://github.com/shinryuzz";
+const zenn: string = "https://zenn.dev/shinryuzz";
 const facebook: string =
   "https://www.facebook.com/profile.php?id=100012067954842";
 const instagram: string = "https://www.instagram.com/shinryuzz__/";
-const note: string = "https://note.com/shinryuzz__/";
+// const note: string = "https://note.com/shinryuzz__/";
 const booklog: string = "https://booklog.jp/users/shinryuzz";
 const scrapbox: string = "https://scrapbox.io/shinryuzz";
 
 export const socialLinks: SocialLink[] = [
   { name: "Twitter", url: twitter },
   { name: "Github", url: github },
+  { name: "Zenn", url: zenn },
   { name: "Facebook", url: facebook },
   { name: "Instagram", url: instagram },
-  { name: "note", url: note },
   { name: "ブクログ", url: booklog },
+  // { name: "note", url: note },
   { name: "Scrapbox", url: scrapbox },
 ];
